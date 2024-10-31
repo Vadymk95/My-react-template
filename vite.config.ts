4f7dff686ab5f822fed4ade7c93baf13fa07d7dd
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-10-31 23:07:26 +0200

    added eslint and svg plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint2';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint(), svgr()],
     base: './',
     build: {
         minify: 'esbuild',
