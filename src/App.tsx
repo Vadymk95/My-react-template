7f3426138f2615cba42f811425b00eb0ca3db440
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-10-31 10:16:39 +0200

    installed vite + react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './App.css';
+
+export const App: FC = () => {
+    return (
+        <div>
+            <h1>Hello My React Template!</h1>
+        </div>
+    );
+};
